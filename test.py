073b429aa97be96e2d6da5ede08625421ae43959
jaychen92123 <chenzewei11103@gmail.com>
2024-07-19 20:52:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import ta
 #import talib
 
 # 登录Shioaji API
